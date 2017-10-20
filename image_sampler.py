89fcd1af53ff4494c4d1fb7e6ee564a60c61773b
nakatsuka <jacketgardensio@gmail.com>
2017-10-20 14:21:59 +0900

    update image_sampler

--- image_sampler.py
+++ image_sampler.py
@@ -5,24 +5,40 @@ from PIL import Image
 
 
 class ImageSampler:
-    def __init__(self, image_dir, target_size=None, color_mode='rgb', normalize_mode='tanh', nb_sample=None):
-        self.image_dir = image_dir
+    def __init__(self, target_size=None, color_mode='rgb', normalize_mode='tanh', is_training=True):
         self.target_size = target_size
         self.color_mode = color_mode
-        self.image_paths = np.array(sorted([path for path in get_image_paths(image_dir)]))
         self.normalize_mode = normalize_mode
+        self.is_training = is_training
+
+    def flow(self, x, y=None, batch_size=32, shuffle=True, seed=None):
+        return ArrayIterator(x, y, target_size=self.target_size,
+                             color_mode=self.color_mode, batch_size=batch_size,
+                             normalize_mode=self.normalize_mode, shuffle=shuffle, seed=seed,
+                             is_training=self.is_training)
+
+    def flow_from_directory(self, image_dir, batch_size=32, shuffle=True, seed=None, nb_sample=None):
+        image_paths = np.array([path for path in get_image_paths(image_dir)])
         if nb_sample is not None:
-            self.image_paths = self.image_paths[:nb_sample]
-        self.nb_sample = len(self.image_paths)
+            image_paths = image_paths[:nb_sample]
 
-    def flow(self, batch_size, shuffle=True, seed=None):
-        return DataIterator(paths=self.image_paths, target_size=self.target_size,
-                            color_mode=self.color_mode, batch_size=batch_size,
-                            normalize_mode=self.normalize_mode, shuffle=shuffle, seed=seed)
+        return DirectoryIterator(paths=image_paths, target_size=self.target_size,
+                                 color_mode=self.color_mode, batch_size=batch_size,
+                                 normalize_mode=self.normalize_mode, shuffle=shuffle, seed=seed,
+                                 is_training=self.is_training)
+
+    def flow_from_path(self, paths, y=None, batch_size=32, shuffle=True, seed=None, nb_sample=None):
+        if nb_sample is not None:
+            image_paths = paths[:nb_sample]
+            labels = y[:nb_sample]
+        else:
+            image_paths = paths
+            labels = y
+        raise NotImplementedError
 
 
-class DataIterator(Iterator):
-    def __init__(self, paths, target_size, color_mode, batch_size, normalize_mode, shuffle, seed):
+class DirectoryIterator(Iterator):
+    def __init__(self, paths, target_size, color_mode, batch_size, normalize_mode, shuffle, seed, is_training):
         self.paths = paths
         self.target_size = target_size
         self.color_mode = color_mode
@@ -31,8 +47,15 @@ class DataIterator(Iterator):
         self.normalize_mode = normalize_mode
         super().__init__(self.nb_sample, batch_size, shuffle, seed)
         self.current_paths = None
+        self.is_trianing = is_training
 
     def __call__(self, *args, **kwargs):
+        if self.is_trianing:
+            return self.flow_on_training()
+        else:
+            return self.flow_on_test()
+
+    def flow_on_training(self):
         with self.lock:
             index_array, current_index, current_batch_size = next(self.index_generator)
         image_path_batch = self.paths[index_array]
@@ -41,26 +64,109 @@ class DataIterator(Iterator):
         self.current_paths = image_path_batch
         return image_batch
 
+    def flow_on_test(self):
+        indexes = np.arange(self.nb_sample)
+        if self.shuffle:
+            print('Now you set is_training = False. \nBut shuffle = True')
+            np.random.shuffle(indexes)
+
+        steps = self.nb_sample // self.batch_size
+        if self.nb_sample % self.batch_size != 0:
+            steps += 1
+        for i in range(steps):
+            index_array = indexes[i * self.batch_size: (i + 1) * self.batch_size]
+            image_path_batch = self.paths[index_array]
+
+            image_batch = np.array([load_image(path, self.target_size, self.color_mode)
+                                    for path in image_path_batch])
+
+            self.current_paths = image_path_batch
+            yield image_batch
+
     def data_to_image(self, x):
         return denormalize(x, self.normalize_mode)
 
 
-def load_image(path, target_size=None, color_mode='rgb'):
-    assert color_mode in ['grayscale', 'gray', 'rgb']
-    image = Image.open(path)
+class ArrayIterator(Iterator):
+    def __init__(self, x, y, target_size, color_mode, batch_size, normalize_mode, shuffle, seed, is_training):
+        self.x = x
+        self.y = y
+        self.target_size = target_size
+        self.color_mode = color_mode
+        self.nb_sample = len(self.x)
+        self.batch_size = batch_size
+        self.normalize_mode = normalize_mode
+        super().__init__(self.nb_sample, batch_size, shuffle, seed)
+        self.is_training = is_training
 
+        if len(self.x.shape) == 4:
+            if x.shape[3] == 1:
+                self.x = self.x.reshape(self.x.shape[:3])
+
+    def __call__(self, *args, **kwargs):
+        if self.is_training:
+            return self.flow_on_training()
+        else:
+            return self.flow_on_test()
+
+    def flow_on_training(self):
+        with self.lock:
+            index_array, current_index, current_batch_size = next(self.index_generator)
+        image_batch = np.array([preprocessing(Image.fromarray(x), color_mode=self.color_mode,
+                                              target_size=self.target_size)
+                                for x in self.x[index_array]])
+        if self.y is not None:
+            label_batch = self.y[index_array]
+            return image_batch, label_batch
+        else:
+            return image_batch
+
+    def flow_on_test(self):
+        indexes = np.arange(self.nb_sample)
+        if self.shuffle:
+            print('Now you set is_training = False. \nBut shuffle = True')
+            np.random.shuffle(indexes)
+
+        steps = self.nb_sample // self.batch_size
+        if self.nb_sample % self.batch_size != 0:
+            steps += 1
+        for i in range(steps):
+            index_array = indexes[i * self.batch_size: (i + 1) * self.batch_size]
+            image_batch = np.array([preprocessing(image, color_mode=self.color_mode,
+                                                  target_size=self.target_size)
+                                    for image in self.x[index_array]])
+            if self.y is not None:
+                label_batch = self.y[index_array]
+                yield image_batch, label_batch
+            else:
+                yield image_batch
+
+    def data_to_image(self, x):
+        return denormalize(x, self.normalize_mode)
+
+
+def preprocessing(x, target_size=None, color_mode='rgb'):
+    assert color_mode in ['grayscale', 'gray', 'rgb']
     if color_mode in ['grayscale', 'gray']:
-        image = image.convert('L')
+        image = x.convert('L')
+    else:
+        image = x
 
     if target_size is not None and target_size != image.size:
         image = image.resize(target_size, Image.BILINEAR)
 
     image_array = np.asarray(image)
+
     image_array = normalize(image_array)
 
     if color_mode in ['grayscale', 'gray']:
         image_array = image_array.reshape(image.size[1], image.size[0], 1)
     return image_array
+
+
+def load_image(path, target_size=None, color_mode='rgb'):
+    image = Image.open(path)
+    return preprocessing(image, target_size, color_mode)
 
 
 def normalize(x, mode='tanh'):
--- mnist/train.py
+++ mnist/train.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import argparse
+import gzip
+import pickle
 sys.path.append(os.getcwd())
 from aae import AdversarialAutoEncoder as AAE
 from image_sampler import ImageSampler
@@ -10,9 +12,17 @@ from mnist.autoencoder import AutoEncoder
 from mnist.discriminator import Discriminator
 
 
+def load_mnist_train(file_path):
+    f = gzip.open(file_path)
+    (train_x, _), (_, _), (_, _) = pickle.load(f, encoding='latin1')
+    train_x = train_x.reshape(len(train_x), 28, 28, 1)
+    train_x = (train_x * 255).astype('uint8')
+    return train_x
+
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('image_dir', type=str)
+    parser.add_argument('mnist_path', type=str)
     parser.add_argument('--batch_size', '-bs', type=int, default=64)
     parser.add_argument('--nb_epoch', '-e', type=int, default=1000)
     parser.add_argument('--latent_dim', '-ld', type=int, default=2)
@@ -33,9 +43,9 @@ def main():
     dump_config(os.path.join(args.result_dir, 'config.csv'), args)
     input_shape = (args.height, args.width, args.channel)
 
-    image_sampler = ImageSampler(args.image_dir,
-                                 target_size=(args.width, args.height),
-                                 color_mode='rgb' if args.channel == 3 else 'gray')
+    image_sampler = ImageSampler(target_size=(args.width, args.height),
+                                 color_mode='rgb' if args.channel == 3 else 'gray',
+                                 is_training=True)
     noise_sampler = NoiseSampler(args.noise_mode)
 
     autoencoder = AutoEncoder(input_shape, args.latent_dim,
@@ -45,7 +55,9 @@ def main():
 
     aae = AAE(autoencoder, discriminator, is_training=True)
 
-    aae.fit_generator(image_sampler.flow(args.batch_size), noise_sampler, nb_epoch=args.nb_epoch,
+    aae.fit_generator(image_sampler.flow(load_mnist_train(args.mnist_path),
+                                         batch_size=args.batch_size),
+                      noise_sampler, nb_epoch=args.nb_epoch,
                       save_steps=args.save_steps, visualize_steps=args.visualize_steps,
                       result_dir=args.result_dir, model_dir=args.model_dir)
 
